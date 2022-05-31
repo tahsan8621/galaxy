68c18748e834d39d7eba0e3dcb8be11aeffe4388
Tanjeeb Ahsan <tanjeeb211@live.com>
2022-06-01 04:31:37 +0600

    add styles

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ const Home = () => {
         let getList = document.getElementById("user").innerText;
 
 
-
         let dynamicStyles = null;
 
         function addAnimation(body) {
@@ -19,20 +18,20 @@ const Home = () => {
         }
 
         for (var i = 1; i <= getList; i++) {
-            if(i%2==0){
+            if (i % 2 == 0) {
                 addAnimation(`
       @keyframes movement${i} { 
-         0%  { -webkit-transform: rotate(0deg) translateY(${40*i}px) rotate(0deg) scale(1); }
-      50%  { -webkit-transform: rotate(-180deg) translateY(${40*i}px) rotate(180deg) scale(1); }
-      100%  { -webkit-transform: rotate(-360deg) translateY(${40*i}px) rotate(360deg) scale(1); }
+         0%  { -webkit-transform: rotate(0deg) translateY(${100 * i}px) rotate(0deg) scale(1); }
+      50%  { -webkit-transform: rotate(-180deg) translateY(${100 * i}px) rotate(180deg) scale(1); }
+      100%  { -webkit-transform: rotate(-360deg) translateY(${100 * i}px) rotate(360deg) scale(1); }
       }
     `);
-            }else{
+            } else {
                 addAnimation(`
       @keyframes movement${i} { 
-         0%  { -webkit-transform: rotate(0deg) translateY(${40*i}px) rotate(0deg) scale(1); }
-      50%  { -webkit-transform: rotate(180deg) translateY(${40*i}px) rotate(-180deg) scale(1); }
-      100%  { -webkit-transform: rotate(360deg) translateY(${40*i}px) rotate(-360deg) scale(1); }
+         0%  { -webkit-transform: rotate(0deg) translateY(${100 * i}px) rotate(0deg) scale(1); }
+      50%  { -webkit-transform: rotate(180deg) translateY(${100 * i}px) rotate(-180deg) scale(1); }
+      100%  { -webkit-transform: rotate(360deg) translateY(${100 * i}px) rotate(-360deg) scale(1); }
       }
     `);
             }
@@ -40,10 +39,8 @@ const Home = () => {
 
             var element = document.createElement("div");
             element.className = "globe ele-1";
-
-            element.style.animation = `3s linear infinite movement${i}`;
-
-
+            element.style.animation = `7s linear infinite movement${i}`;
+            element.textContent=`user-${i}`;
             document.getElementById("atom").appendChild(element);
         }
     }, []);
@@ -51,14 +48,13 @@ const Home = () => {
     return (
 
         <div id="atom">
-
+            Currently Active
             <div id="user">
                 10
             </div>
 
 
-        </div>
-    );
+        </div>);
 
 }
 export default Home;
