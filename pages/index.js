4bd12c7de1ad8fe603f548f2f7e702094d9efd37
Tanjeeb Ahsan <tanjeeb211@live.com>
2022-06-01 22:24:48 +0600

    fixed first user position

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,8 @@ const Home = ({data}) => {
     const [user, setUser] = useState([])
 
 
-    useEffect( () => {
-
-        const newUserData=data.devices;
+    useEffect(() => {
+        const newUserData = data.devices;
         setUser(newUserData)
         console.log(user[0]?.name)
         let getList = document.getElementById("user").innerText;
@@ -25,10 +24,10 @@ const Home = ({data}) => {
             dynamicStyles.sheet.insertRule(body, dynamicStyles.length);
         }
 
-        for (var i = 0; i < getList; i++) {
+        for (var i = 1; i <= getList; i++) {
             if (i % 2 == 0) {
                 addAnimation(`
-                  @keyframes movement_${i} { 
+                  @keyframes movement_${i} {
                      0%  { -webkit-transform: rotate(0deg) translateY(${100 * i}px) rotate(0deg) scale(1); }
                   50%  { -webkit-transform: rotate(-180deg) translateY(${100 * i}px) rotate(180deg) scale(1); }
                   100%  { -webkit-transform: rotate(-360deg) translateY(${100 * i}px) rotate(360deg) scale(1); }
@@ -36,7 +35,7 @@ const Home = ({data}) => {
                 `);
             } else {
                 addAnimation(`
-                  @keyframes movement_${i} { 
+                  @keyframes movement_${i} {
                      0%  { -webkit-transform: rotate(0deg) translateY(${100 * i}px) rotate(0deg) scale(1); }
                   50%  { -webkit-transform: rotate(180deg) translateY(${100 * i}px) rotate(-180deg) scale(1); }
                   100%  { -webkit-transform: rotate(360deg) translateY(${100 * i}px) rotate(-360deg) scale(1); }
@@ -46,9 +45,9 @@ const Home = ({data}) => {
 
 
             var element = document.createElement("div");
-            element.className = "globe ele-1";
+            element.className = "globe";
             element.style.animation = `7s linear infinite movement_${i}`;
-            element.textContent = `${user[i]?.name}`;
+            element.textContent = `${user[i-1]?.name}`;
             document.getElementById("atom").appendChild(element);
         }
     });
@@ -67,11 +66,15 @@ const Home = ({data}) => {
 }
 
 export async function getServerSideProps() {
+
+
     // Fetch data from external API
     const res = await fetch(`http://35.201.2.209:8000/devices`)
+    //setInterval(res, 1000);
     const data = await res.json()
     console.log(data.devices)
     // Pass data to the page via props
-    return { props: { data } }
+    return {props: {data}}
 }
+
 export default Home;
